3510b47bcd4b1cd18a188c1170b9f01b78ce8d99
Nathaniel Aquino <naquino14@outlook.com>
2022-10-28 20:43:06 -0400

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import PageContainer from './containers/PageContainer'
+import 'csh-material-bootstrap/dist/csh-material-bootstrap.css'
+import NotFound from './pages/NotFound'
 
-function App() {
+type Props = {
+  rerouteHomeOn404?: boolean
+}
+
+const App: React.FC<Props> = ({ rerouteHomeOn404 = null }) => {
   return (
-    <div>
-      Hello World!
-    </div>
+    <Router>
+      <PageContainer>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='*' element={rerouteHomeOn404 ?? true ? <Home /> : <NotFound />} />
+        </Routes>
+      </PageContainer>
+    </Router>
   )
 }
 
